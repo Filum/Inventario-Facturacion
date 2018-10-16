dc27ca52ab6ff55fcea621395673a2edd70d2faa
Vinicio Pacheco Vargas <vipava.13@gmail.com>
2018-10-16 13:47:41 -0600

    Buscar y actualizar funcionales con todos los datos

--- Proyecto/Datos/Data.cs
+++ Proyecto/Datos/Data.cs
@@ -141,8 +141,8 @@ namespace Datos
                     EntidadClientes cliente = new EntidadClientes();
                     cliente.v_Codigo = dr.GetInt32(0);
                     cliente.v_NombreCompleto = dr.GetString(2);
-                    //cliente.v_Teleoficina = dr.GetInt32(3);
-                    //cliente.v_Telemovil = dr.GetInt32(4);
+                    cliente.v_Teleoficina = Convert.ToInt32(dr.GetValue(3));
+                    cliente.v_Telemovil = Convert.ToInt32(dr.GetValue(4));
                     cliente.v_Correo = dr.GetString(5);
                     cliente.v_CorreoOpc = dr.GetString(6);
                     cliente.v_Inactividad = dr.GetInt32(7);
